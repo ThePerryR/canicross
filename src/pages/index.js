89b5cb3e2f1b3c9501eef67c6ccd03266bf9df19
theperryr <theperryr@gmail.com>
2019-02-19 09:14:28 -0600

    add horsey gif

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
+import horse from './horse.gif'
+
 const IndexPage = () => (
   <div
     style={{
@@ -15,6 +17,7 @@ const IndexPage = () => (
       alignItems: 'center',
       justifyContent: 'center'
     }}>
+    <img src={horse} style={{height: 120}}/>
     <h3>Can I cross <a href="https://www.delmarblvd.com">delmar</a> on a horse?</h3>
     <h1>No!!!</h1>
   </div>
