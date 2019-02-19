66d930a2da3d1326a9d3e48efa0ccfde80d2d3cc
theperryr <theperryr@gmail.com>
2019-02-19 09:06:09 -0600

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div
+    style={{
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+    <h3>Can I cross <a href="https://www.delmarblvd.com">delmar</a> on a horse?</h3>
+    <h1>No!!!</h1>
+  </div>
 )
 
 export default IndexPage
